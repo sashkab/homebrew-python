230b9f240efbe754a2b950a17bcd8c11f00810ee
Matthew Raspberry <nixalot@users.noreply.github.com>
2017-02-24 20:25:18 -0600

    python3: add dtrace support
    Closes #10343.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- python36.rb
+++ python36.rb
@@ -4,6 +4,7 @@ class Python36 < Formula
   url "https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz"
   sha256 "b0c5f904f685e32d9232f7bdcbece9819a892929063b6e385414ad2dd6a23622"
   head "https://github.com/python/cpython", :using => :git
+  revision 1
 
   option :universal
   option "with-tcl-tk", "Use Homebrew's Tk instead of macOS Tk (has optional Cocoa and threads support)"
@@ -80,6 +81,7 @@ class Python36 < Formula
       --datadir=#{share}
       --enable-framework=#{frameworks}
       --without-ensurepip
+      --with-dtrace
     ]
 
     args << "--without-gcc" if ENV.compiler == :clang
