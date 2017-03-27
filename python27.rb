5340a2a6633ea5e3122ff54816dc94553078c79b
Aleks Bunin <github@compuix.com>
2017-03-27 15:41:41 -0400

    WIP -- conflict with python.

--- python27.rb
+++ python27.rb
@@ -4,6 +4,9 @@ class Python27 < Formula
   url "https://www.python.org/ftp/python/2.7.13/Python-2.7.13.tar.xz"
   sha256 "35d543986882f78261f97787fd3e06274bfa6df29fac9b4a94f73930ff98f731"
   head "https://github.com/python/cpython.git", :branch => "2.7"
+  revision 2
+
+  conflicts_with "python", :because => "it replaces python formulae." 
 
   # Please don't add a wide/ucs4 option as it won't be accepted.
   # More details in: https://github.com/Homebrew/homebrew/pull/32368
@@ -355,6 +358,6 @@ class Python27 < Formula
     system "#{bin}/python", "-c", "import sqlite3"
     # Check if some other modules import. Then the linked libs are working.
     system "#{bin}/python", "-c", "import Tkinter; root = Tkinter.Tk()"
-    system bin/"pip", "list"
+    system bin/"pip2.7", "list"
   end
 end
