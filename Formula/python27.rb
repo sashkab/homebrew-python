f978bfb8651be8f48e6b2a07a4e045b86ec38681
Aleks Bunin <github@compuix.com>
2019-03-19 09:39:53 -0400

    pip 19.0.3; setuptools 40.8.0; wheel 0.33.1.

--- Formula/python27.rb
+++ Formula/python27.rb
@@ -27,18 +27,18 @@ class Python27 < Formula
   depends_on "sqlite"
 
   resource "setuptools" do
-    url "https://pypi.org/packages/source/s/setuptools/setuptools-40.6.3.zip"
-    sha256 "3b474dad69c49f0d2d86696b68105f3a6f195f7ab655af12ef9a9c326d2b08f8"
+    url "https://pypi.org/packages/source/s/setuptools/setuptools-40.8.0.zip"
+    sha256 "6e4eec90337e849ade7103723b9a99631c1f0d19990d6e8412dc42f5ae8b304d"
   end
 
   resource "pip" do
-    url "https://www.pypi.org/packages/source/p/pip/pip-18.1.tar.gz"
+    url "https://www.pypi.org/packages/source/p/pip/pip-19.0.3.tar.gz"
     sha256 "c0a292bd977ef590379a3f05d7b7f65135487b67470f6281289a94e015650ea1"
   end
 
   resource "wheel" do
-    url "https://pypi.org/packages/source/w/wheel/wheel-0.32.3.tar.gz"
-    sha256 "029703bf514e16c8271c3821806a1c171220cc5bdd325cbf4e7da1e056a01db6"
+    url "https://pypi.org/packages/source/w/wheel/wheel-0.33.1.tar.gz"
+    sha256 "66a8fd76f28977bb664b098372daef2b27f60dc4d1688cfab7b37a09448f0e9d"
   end
 
   def lib_cellar
