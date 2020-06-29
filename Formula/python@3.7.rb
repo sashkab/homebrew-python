6f2b8ef823e2be4d82fcbd6f7a99f702bc3bf364
Aleks Bunin <github@compuix.com>
2020-06-29 12:03:49 -0400

    python@3.7: update setuptools and pip

--- Formula/python@3.7.rb
+++ Formula/python@3.7.rb
@@ -34,13 +34,13 @@ class PythonAT37 < Formula
   skip_clean "bin/easy_install3", "bin/easy_install-3.4", "bin/easy_install-3.5",
              "bin/easy_install-3.6", "bin/easy_install-3.7"
   resource "setuptools" do
-    url "https://pypi.org/packages/source/s/setuptools/setuptools-46.0.0.zip"
-    sha256 "2f00f25b780fbfd0787e46891dcccd805b08d007621f24629025f48afef444b5"
+    url "https://pypi.org/packages/source/s/setuptools/setuptools-47.3.1.zip"
+    sha256 "843037738d1e34e8b326b5e061f474aca6ef9d7ece41329afbc8aac6195a3920"
   end
 
   resource "pip" do
-    url "https://www.pypi.org/packages/source/p/pip/pip-20.0.2.tar.gz"
-    sha256 "7db0c8ea4c7ea51c8049640e8e6e7fde949de672bfa4949920675563a5a6967f"
+    url "https://www.pypi.org/packages/source/p/pip/pip-20.1.1.tar.gz"
+    sha256 "27f8dc29387dd83249e06e681ce087e6061826582198a425085e0bf4c1cf3a55"
   end
 
   resource "wheel" do
