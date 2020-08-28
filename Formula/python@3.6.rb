491c6fb0b76165bebcde0af2d5060acc55e2c07c
Aleks Bunin <github@compuix.com>
2020-08-28 15:05:11 -0400

    python@3.6: Python 3.6.12

--- Formula/python@3.6.rb
+++ Formula/python@3.6.rb
@@ -1,8 +1,8 @@
 class PythonAT36 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
-  url "https://www.python.org/ftp/python/3.6.11/Python-3.6.11.tar.xz"
-  sha256 "741ebdcbc4e3937a5ff23517dd455ebf7d543ea9fef6f5cf6f46e575d6c4fda4"
+  url "https://www.python.org/ftp/python/3.6.12/Python-3.6.12.tar.xz"
+  sha256 "70953a9b5d6891d92e65d184c3512126a15814bee15e1eff2ddcce04334e9a99"
   head "https://github.com/python/cpython.git", branch: "3.6"
 
   # setuptools remembers the build flags python is built with and uses them to
@@ -29,18 +29,18 @@ class PythonAT36 < Formula
   skip_clean "bin/easy_install3", "bin/easy_install-3.4", "bin/easy_install-3.5", "bin/easy_install-3.6"
 
   resource "setuptools" do
-    url "https://pypi.org/packages/source/s/setuptools/setuptools-49.2.0.zip"
-    sha256 "afe9e81fee0270d3f60d52608549cc8ec4c46dada8c95640c1a00160f577acf2"
+    url "https://pypi.org/packages/source/s/setuptools/setuptools-49.6.0.zip"
+    sha256 "46bd862894ed22c2edff033c758c2dc026324788d758e96788e8f7c11f4e9707"
   end
 
   resource "pip" do
-    url "https://www.pypi.org/packages/source/p/pip/pip-20.1.1.tar.gz"
-    sha256 "27f8dc29387dd83249e06e681ce087e6061826582198a425085e0bf4c1cf3a55"
+    url "https://www.pypi.org/packages/source/p/pip/pip-20.2.2.tar.gz"
+    sha256 "58a3b0b55ee2278104165c7ee7bc8e2db6f635067f3c66cf637113ec5aa71584"
   end
 
   resource "wheel" do
-    url "https://pypi.org/packages/source/w/wheel/wheel-0.34.2.tar.gz"
-    sha256 "8788e9155fe14f54164c1b9eb0a319d98ef02c160725587ad60f14ddc57b6f96"
+    url "https://pypi.org/packages/source/w/wheel/wheel-0.35.1.tar.gz"
+    sha256 "99a22d87add3f634ff917310a3d87e499f19e663413a52eb9232c447aa646c9f"
   end
 
   def install
