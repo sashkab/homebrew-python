6c9d2fec91edb0f34b0bc64978a34e9ad095a5bb
Aleks Bunin <github@compuix.com>
2020-08-28 20:21:49 -0400

    python@3.6: fix license

--- Formula/python@3.6.rb
+++ Formula/python@3.6.rb
@@ -3,8 +3,8 @@ class PythonAT36 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.6.12/Python-3.6.12.tar.xz"
   sha256 "70953a9b5d6891d92e65d184c3512126a15814bee15e1eff2ddcce04334e9a99"
-  head "https://github.com/python/cpython.git", branch: "3.6"
   license "Python-2.0"
+  head "https://github.com/python/cpython.git", branch: "3.6"
 
   livecheck do
     url "https://www.python.org/ftp/python/"
