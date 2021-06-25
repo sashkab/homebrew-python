8c7b60472357f2a57b2c476519daf1db088b2f9f
Aleks Bunin <868842+sashkab@users.noreply.github.com>
2021-06-25 16:52:11 +0000

    python@3.8: update 3.8.10 bottle.

--- Formula/python@3.8.rb
+++ Formula/python@3.8.rb
@@ -11,8 +11,8 @@ class PythonAT38 < Formula
   end
 
   bottle do
-    root_url "https://github.com/sashkab/homebrew-python/releases/download/python@3.8-3.8.8"
-    sha256 catalina: "801de2f653296f48e7fdb0019452378a0f8a67b09d175c527ad1c9faeac169ab"
+    root_url "https://github.com/sashkab/homebrew-python/releases/download/python@3.8-3.8.10"
+    sha256 catalina: "7e03eb2f607689311b9542c18e5e581640e509850ee7b58a2b948ca91cba34fd"
   end
 
   # setuptools remembers the build flags python is built with and uses them to
